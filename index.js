a5ed7d48ebb8a7dd4ea9a27849398e9d34b527d3
Sylviachelimo <chelimosylvia280@gmail.com>
2024-01-16 14:15:30 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    let num1 = 2000
+    let num2 = 100
+    return num1 / num2
+}
+function square() {
+    let parameter1 = 4
+    return parameter1 
+}
+function add() {
+    let parameter1 = 3
+    let parameter2 = 4
+    return parameter1 + parameter2
+}
